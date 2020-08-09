152ee9affff433f8ba542895d252790eb867e9d0
Malcolm Leake <echelon1089@gmail.com>
2020-08-09 03:23:00 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,5 +1,12 @@
-// import React, {Component} from 'react'
-//
-// export default class ImageSlider extends Component {
-//
-// }
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+  constructor(props) {
+    super();
+    this.state = { currentSlideIndex: 0 };
+  }
+
+  render() {
+    return `I am on slide ${this.state.currentSlideIndex}`;
+  }
+}
